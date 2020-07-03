debffab8bf762634a8f7aa8416f5e7aa417a31ae
Brandon Smith <brandonsmith1197@gmail.com>
2020-07-02 20:56:42 -0400

    [ADD] added final apis to liri.js and completed functionality.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LiriBot
+
+In this app liri bot, you can either Concert-This which will let you see upcomming events and information on whichever artist is inputted. Movie-this which will bring you a list of information about the inputed movie. And spotify-this-song to see a list of the artist, song, and lyrics info all through diffrent apis implemented into the LiriBot.
--- liri.js
+++ liri.js
@@ -1,5 +1,51 @@
+var Spotify = require('node-spotify-api');
 require("dotenv").config();
-
 var keys = require("./keys.js");
-
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios")
+console.log(process.argv)
+var command = process.argv[2]
+var input = process.argv.slice(3).join(" ")
+
+console.log(command)
+console.log(input)
+
+if (command === "concert-this") {
+    console.log("this is the concert test")
+    axios({
+
+        url: "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp",
+        method: "get"
+    }).then(function (response) {
+
+        console.log(response.data)
+
+    })
+} else if (command === "movie-this") {
+    console.log("this is a movie test")
+
+    axios({
+
+        url: "http://www.omdbapi.com/?apikey=trilogy&t=" + input,
+
+        method: "get"
+    }).then(function (response) {
+
+        console.log(response.data)
+
+    })
+
+} else if (command === "spotify-this-song") {
+    console.log("this is a song test")
+
+    spotify.search({
+        type: 'track',
+        query: input
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data.tracks);
+    });
+}
